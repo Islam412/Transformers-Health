cbcf6fc20c76e520ea51881bcf08b6f72767edcc
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-25 01:57:42 +0200

    create kyc_registration url

--- userauths/urls.py
+++ userauths/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('sign-up/', views.register_view, name='sign-up'),
     path('sign-in/', views.login_view, name='sign-in'),
     path('sign-out/', views.logout_view, name='sign-out'),
+    path('profile-edit/', views.kyc_registration, name='kyc-registration'),
 
 ]
