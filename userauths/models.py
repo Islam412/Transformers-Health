bb7536c8588d201067a3f265a54072329c57ecca
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-25 01:25:56 +0200

    desing profile models

--- userauths/models.py
+++ userauths/models.py
@@ -76,3 +76,20 @@ def save_account(sender, instance,**kwargs):
 
 post_save.connect(create_account, sender=User)
 post_save.connect(save_account, sender=User)
+
+
+
+class KYC(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    user =  models.OneToOneField(User, on_delete=models.CASCADE)
+    account =  models.OneToOneField(Account, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(upload_to="kyc", default="default.jpg")
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.user}"    
+
+    
+    class Meta:
+        ordering = ['-date']
