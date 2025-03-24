3a348e69151199e283bf00f1f6734a1eac875dd1
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-25 01:21:04 +0200

    desing user models

--- userauths/models.py
+++ userauths/models.py
@@ -17,3 +17,21 @@ def user_directory_path(instance, filename):
     return "user_{0}/{1}".format(instance.user.id, filename)
 
 
+
+class User(AbstractUser):
+    frist_name = models.CharField(_('Frist Name'), max_length=255)
+    last_name = models.CharField(_('Last Name'), max_length=255)
+    username = models.CharField(_('Username'), max_length=255)
+    email = models.EmailField(_('Email'), unique=True)
+    phone = models.CharField(_("Phone Number"),max_length=50, null=True, blank=True)
+    company = models.CharField(_('Company'), max_length=255, null=True, blank=True)
+    date_of_birth = models.DateTimeField(_('Date Of Birth'), auto_now_add=False, blank=True, null=True)
+    is_staff = models.BooleanField(_('Is Staff'), default=False)
+    is_superuser = models.BooleanField(_('Super User'), default=False)
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return str(self.username)
