4a5f58d4dabfe657c3beb524fac05430a4d81657
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-25 01:51:22 +0200

    create register view

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate, login, logout , update_session_auth_hash
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+
+from .models import User , Account , KYC
+from .forms import UserRegisterForm  , UserPasswordChangeForm , UserKYCForm
+
 
 # Create your views here.
+
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in.")
+        return redirect("userauths:account")
+    
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.username = form.cleaned_data['username']  
+            new_user.save()
+            
+            messages.success(request, f'Account created for {new_user.username}! You are now able to log in.')
+            
+            new_user = authenticate(
+                request,
+                username=form.cleaned_data['email'],
+                password=form.cleaned_data['password1'],
+            )
+            
+            if new_user is not None:
+                login(request, new_user)
+                return redirect("userauths:account")
+            else:
+                messages.error(request, "Authentication failed. Please log in manually.")
+                return redirect("userauths:login")
+    else:
+        form = UserRegisterForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'userauths/sign-up.html', context)
