7d399b3844b30dead416ba36cf0454f974bfff6e
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-25 01:20:23 +0200

    create user_directory_path models

--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.utils.translation import gettext_lazy as _
+
+
+import uuid
+from shortuuid.django_fields import ShortUUIDField
+
 
 # Create your models here.
+
+
+def user_directory_path(instance, filename):
+    ext = filename.split(".")[-1]
+    filename = "%s_%s" % (instance.id, ext)
+    return "user_{0}/{1}".format(instance.user.id, filename)
+
+
