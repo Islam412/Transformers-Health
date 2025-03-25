fcaf265a67d0ba7ea1164ac86f2b826e8d0edafa
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-25 02:00:09 +0200

    desing account url

--- userauths/urls.py
+++ userauths/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('sign-in/', views.login_view, name='sign-in'),
     path('sign-out/', views.logout_view, name='sign-out'),
     path('profile-edit/', views.kyc_registration, name='kyc-registration'),
+    path('account/', views.account, name='account'),
 
 ]
