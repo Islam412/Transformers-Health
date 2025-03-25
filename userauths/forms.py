8f2fd535bf258f220956fcc1f3f763387eaadb6a
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-25 02:08:00 +0200

    desing user kyc form

--- userauths/forms.py
+++ userauths/forms.py
@@ -20,3 +20,51 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'phone', 'date_of_birth', 'company', 'password1', 'password2']
+
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+
+class UserKYCForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "First Name"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Last Name"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Username"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email" , "readonly": "readonly"}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Phone"}), required=False)
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}), required=False)
+    company = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Company"}), required=False)
+
+    # data kyc models
+    image = forms.ImageField(widget=FileInput, required=False)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'phone', 'date_of_birth', 'company', 'image']
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+        if user:
+            self.fields['first_name'].initial = user.first_name
+            self.fields['last_name'].initial = user.last_name
+            self.fields['username'].initial = user.username
+            self.fields['email'].initial = user.email
+            self.fields['phone'].initial = user.phone
+            self.fields['date_of_birth'].initial = user.date_of_birth
+            self.fields['company'].initial = user.company
+
+            if hasattr(user, 'kyc'):
+                self.fields['image'].initial = user.kyc.image
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+            kyc, created = KYC.objects.get_or_create(user=user)
+            kyc.image = self.cleaned_data.get('image')
+            kyc.save()
+        return user
